1afcf4d103d428a98c4df83176b4b746556c1474
armytank <p.armytank@gmail.com>
2016-07-17 16:14:27 +0000

    finished site

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ var express = require('express');
 var app = express();
 var moment = require('moment');
 app.get('/',function(req,res){
-  res.end('Hello World');
+  res.end('Please input a date or a unix timestamp');
 })
 app.get('/:input', function(req,res){
   var string = req.params.input;
+  var natural = null;
+  var unix = null;
   if(!isNaN(string)){
     string = Number(string) * 1000;
   }
   if(moment(string, 'MMM DD, YYYY').isValid()){
-    var date = moment(string).toDate().toString();
-    var unix = moment(string).toDate().getTime()/1000;
-    res.end(date + ', ' + unix);
+    natural = moment(string).format('MMM DD, YYYY');
+    unix = moment(string).toDate().getTime()/1000;
   }
-  res.end('Not a valid date');
+  res.end(JSON.stringify({"unix":unix, "natural":natural}));
 })
 app.listen(process.env.PORT, process.env.IP);
