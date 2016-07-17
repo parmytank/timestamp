6465d9c50a0a635e5324863ba8d4d808c0c8f8a7
armytank <p.armytank@gmail.com>
2016-07-17 14:59:57 +0000

    Sunday morning

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var moment = require('moment');
+app.get('/',function(req,res){
+  res.end('Hello World');
+})
+app.get('/:input', function(req,res){
+  var string = req.params.input;
+  if(!isNaN(string)){
+    string = Number(string) * 1000;
+  }
+  if(moment(string, 'MMM DD, YYYY').isValid()){
+    var date = moment(string).toDate().toString();
+    res.end(date);
+  }
+  res.end('Not a valid date');
+})
+app.listen(process.env.PORT, process.env.IP);
